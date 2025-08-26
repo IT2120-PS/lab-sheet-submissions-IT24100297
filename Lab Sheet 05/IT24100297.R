7d6e020a8654c0f5afb4fc64d933cb63ebc4a7b8
IT24100297 <it24100297@my.sliit.lk>
2025-08-26 16:52:39 +0530

    Lab 5

---
+++ Lab Sheet 05/IT24100297.R
@@ -0,0 +1,38 @@
+#Q1
+setwd("C:\\Users\\IT24100297\\Desktop\\Lab5")
+
+DeliveryTimes <- read.table("Exercise - Lab 05.txt", header=TRUE,sep="", stringsAsFactors = FALSE)
+
+head(DeliveryTimes)
+
+#Q2
+names(DeliveryTimes) <- c("DeliveryTimes")
+hist(DeliveryTimes$DeliveryTime,
+     main = "Histogram for Delivery Times",
+     xlab = "Delivery Time (minutes)",
+     breaks = seq(20, 70, length.out = 10),
+     right = FALSE,
+     col = "lightblue",
+     border = "black")
+
+#Q4
+names(DeliveryTimes)
+x <- as.numeric(DeliveryTimes[[1]])
+hist(x,
+     main = "Histogram for Delivery Times",
+     xlab = "Delivery Time (minutes)",
+     breaks = seq(20, 70, length.out = 10),
+     right = FALSE,
+     col = "lightblue",
+     border = "black")
+
+h <- hist(x, breaks = seq(20, 70, length.out = 10), right = FALSE, plot = FALSE)
+cf <- cumsum(h$counts)
+
+plot(h$breaks[-1], cf, type = "o",
+     main = "Ogive (Cumulative Frequency Polygon)",
+     xlab = "Delivery Time (minutes)",
+     ylab = "Cumulative Frequency",
+     pch = 16)
+points(h$breaks[1], 0, pch = 16)
+lines(c(h$breaks[1], h$breaks[-1], c(0), cf)
